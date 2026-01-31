5fc02377347e7a468cdc9f6fe1e59470207e4ad5
Julien LENORMAND <lenormand@julien0@gmail.com>
2026-01-31 08:29:06 +0100

    feat: add current slide number

---
+++ slidev/global-top.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useNav } from '@slidev/client'
+
+const { currentPage, total } = useNav()
+</script>
+
+<template>
+  <div class="slide-indicator">
+    Slide {{ currentPage }} / {{ total }}
+  </div>
+</template>
+
+<style scoped>
+.slide-indicator {
+  position: fixed;
+  top: 1rem;
+  right: 1rem;
+  font-family: monospace;
+  font-size: 0.8rem;
+  opacity: 0.3;
+  z-index: 100;
+}
+</style>
