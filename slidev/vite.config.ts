ef74bb483f9c32d8f7e22004bc02425a8e0050bc
Julien LENORMAND <lenormand@julien0@gmail.com>
2026-03-31 10:42:15 +0200

    fix: display background images or e,bedded in TS components

---
+++ slidev/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [{ src: 'images', dest: '.' }],
+    }),
+  ],
+})
